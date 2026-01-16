3a915b37cbc07c56f634e2205f24dd369ffdcf34
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-01-16 20:22:14 +0530

    setup for canteen website

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./screens/Home";
+import RestaurantDetails from "./screens/RestaurantDetails";
+import Cart from "./screens/Cart";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/res" element={<RestaurantDetails />} />
+      <Route path="/cart" element={<Cart />} />
+    </Routes>
+  );
+};
+
+export default App;
